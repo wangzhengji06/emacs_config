2d0ade443c6a3007d1b33b2f8f70890b672e8427
wangzhengji06 <wangzhengji06@gmail.com>
2026-05-17 00:14:43 +0900

    For my emacs configuration

---
+++ init.el
@@ -0,0 +1,173 @@
+(require 'package)
+(package-initialize)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/")) 
+
+(use-package emacs
+  :ensure nil
+  :demand t
+  :init
+  (defun prot/keyboard-quit-dwim ()
+    "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+
+The generic `keyboard-quit' does not do the expected thing when
+the minibuffer is open.  Whereas we want it to close the
+minibuffer, even without explicitly focusing it.
+
+The DWIM behaviour of this command is as follows:
+
+- When the region is active, disable it.
+- When a minibuffer is open, but not focused, close the minibuffer.
+- When the Completions buffer is selected, close it.
+- In every other case use the regular `keyboard-quit'."
+    (interactive)
+    (cond
+     ((region-active-p)
+      (keyboard-quit))
+     ((derived-mode-p 'completion-list-mode)
+      (delete-completion-window))
+     ((> (minibuffer-depth) 0)
+      (abort-recursive-edit))
+     (t
+      (keyboard-quit))))
+  :bind
+  ("C-g" . prot/keyboard-quit-dwim)
+  :config
+  (setq frame-resize-pixelwise t)
+  ;; Set your favourite font family and height here.  The :height is
+  ;; 10x the point size you most commonly find on other applications.
+  (set-face-attribute 'default nil :family "0xProto" :height 200)
+  ;; Set your favourite font for elements that are designed to always
+  ;; be monospaced.  The height SHOULD BE a floating point, which is
+  ;; interpreted as relative to the `default'.
+  (set-face-attribute 'fixed-pitch nil :family "" :height 1.0)
+  ;; Same as above for proportionately spaced elements.  Make any
+  ;; buffer proportionately spaced by enabling the `variable-pitch-mode'.
+  ;;
+  ;; [ NOTE: If you use the Modus themes or derivatives, set
+  ;;   `modus-themes-mixed-fonts', load the theme for the option to
+  ;;   take effect, and then enable `variable-pitch-mode':
+  ;;   spacing-sensitive elements like Org tables and code blocks will
+  ;;   remain monospaced. ]
+  (set-face-attribute 'variable-pitch nil :family "0xProto" :height 1.0)
+
+  ;; I have never seen a user say "no" to loading a theme they have
+  ;; downloaded.  Technically, any Elisp file can run arbitrary code,
+  ;; so this is not doing much on the security front.
+  (setq custom-safe-themes t)
+  (setq use-short-answers t)
+  (setq read-answer-short t)
+  (setq help-window-select t) ; also check `display-buffer-alist' below
+  (setq help-window-keep-selected t) ; Emacs 29
+  (setq find-library-include-other-files nil) ; Emacs 29
+  (setq window-combination-resize t)
+  (setq save-interprogram-paste-before-kill t)
+  ;; Do not jump to the current line in `*occur*' buffers.  The reason
+  ;; is that you are already on that line: you want to do `occur' to
+  ;; get more than that (and, presumably, to do something with the
+  ;; results such as to edit them with `occur-edit-mode').
+  (setq list-matching-lines-jump-to-current-line nil)
+  (setq completion-category-defaults nil)) 
+
+;; By default, Emacs will save some state to the bottom of your
+;; init.el.  This can be confusing, so we want to put all such data in
+;; a file called custom.el relative to your init.el
+(use-package custom
+  :ensure nil
+  :config
+  (setq custom-file (locate-user-emacs-file "custom.el"))
+  (load custom-file :no-error-if-missing))
+
+(use-package vertico
+  :ensure t ; install it, if missing
+  :config
+  (vertico-mode 1))
+
+(use-package savehist
+  :ensure nil
+  :config
+  (savehist-mode 1))
+
+(use-package marginalia
+  :ensure t ; install it, if missing
+  :config
+  (marginalia-mode 1))
+
+(use-package orderless
+  :ensure t
+  :config
+  (setq completion-styles '(orderless basic))
+  (setq completion-category-defaults nil))
+
+(use-package corfu
+  :ensure t
+  :init
+  (setq tab-always-indent 'complete)
+  :config
+  (global-corfu-mode 1)
+  (setq corfu-popupinfo-delay '(1.25 . 0.5))
+  (corfu-popupinfo-mode 1)
+  ;; Sort by input history (no need to modify `corfu-sort-function').
+  (with-eval-after-load 'savehist
+    (corfu-history-mode 1)
+    (add-to-list 'savehist-additional-variables 'corfu-history)))
+
+(use-package display-line-numbers
+  :ensure nil
+  :config
+  (global-display-line-numbers-mode 1))
+
+(use-package consult
+  :ensure t
+  :bind
+  ( :map global-map
+    ("C-x b" . consult-buffer)
+    ("C-s" .  consult-line)
+    ("M-s g" . consult-ripgrep)
+    ("M-s f" . consult-find)
+    ("M-s s" . consult-outline))
+  :config
+  (consult-customize consult-find :sort t :state (consult--file-preview)))
+
+(use-package markdown-mode
+  :ensure t)
+
+(use-package eglot
+  :ensure nil)
+
+(use-package python
+  :ensure nil
+  :hook
+  (python-mode . eglot-ensure)) 
+
+(use-package ruff-format
+  :ensure t
+  :hook
+  (python-mode . ruff-format-on-save-mode))
+
+(use-package dape
+  :ensure t)
+
+(use-package csv-mode
+  :ensure t) 
+
+(use-package delsel
+  :ensure nil
+  :config
+  (delete-selection-mode 1))
+
+(use-package envrc
+  :ensure t
+  :config
+  (envrc-global-mode 1))
+
+(use-package org
+  :ensure nil
+  :config
+  ;; put your tasks.org or such files here
+  (setq org-directory "~/org/")
+  ;; M-x org-agenda to read from your files
+  (setq org-agenda-files (list org-directory))) 
+
+
+(load-theme 'modus-vivendi-tinted)
